2d4cd03620265de47b550ad41c8f8f27d13efa4f
heyPreeti-dev <preetiratrey0106@gmail.com>
2025-08-27 20:19:14 +0530

    commit msg

---
+++ first.py
@@ -0,0 +1,46 @@
+# print("my first program")
+
+# age =25
+# name ="preeti"
+# print(age, type(age))
+# print(name, type(name))
+# a ="preeti"
+# b= 34
+# print(a, type(a))
+# print(b, type(b))
+
+# a= "23"
+# b = int(a)
+# c = float(b)
+# print(a)
+# print(b)
+# print(c)
+
+# sbji = ["e", "b", "c"]
+# sbji.append("d")
+# print(sbji, type(sbji))
+
+# pagal = ["pq", "pe" "po" "oi"]
+# pagal.remove("pq")
+# print(pagal, type(pagal))
+
+# p =(10, 10.5 , 10.5, ky, bc)
+# p.append("hk")
+# print(p, type(p))
+
+# print(p, type(p))
+
+
+#Class - 4
+#Escape Sequence charecter
+print("hey", 5, 6, 7)
+
+print("hey", 5, 6, 7, sep="~")
+
+
+print("hey", 5, 6, 7, sep="~", end="0009")
+
+print("hey", 5, 6, 7, sep="~", end="0009\n")
+print("preeti")
+
+#03/08/2025
